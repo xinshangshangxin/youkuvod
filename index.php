fd2d312ddfa797bd20923b4e8312a30d774fe34a
xinshangshangxin <xinshangshangxin@gmail.com>
2015-02-20 14:54:32 +0800

    php环境变量设置

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
 	$content = $_SERVER['QUERY_STRING'];
-	$url = 'http://api.flvxz.com/token/$_ENV["flvxz_token"]/url/'.$content.'/jsonp/prettyjson';
+	$url = 'http://api.flvxz.com/token/'.$_ENV["flvxz_token"].'/url/'.$content.'/jsonp/prettyjson';
 	echo file_get_contents($url);
 ?>
